93c6d65414df9acb2abcc112eb15914fef4bca59
Kamal Kumar <kamalkumar1027@yahoo.com>
2019-05-11 11:58:25 -0400

    added props

--- src/wizard/wizard.js
+++ src/wizard/wizard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Wizard extends Component{
   render(){
@@ -9,3 +10,10 @@ export default class Wizard extends Component{
     )
   }
 }
+
+Wizard.propTypes = {
+  header: PropTypes.func.isRequired,
+  steps: PropTypes.array.isRequired,
+  wizardContext: PropTypes.object.isRequired,
+  onComplete: PropTypes.func.isRequired
+};
